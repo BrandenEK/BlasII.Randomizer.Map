af1a1ae3f298314868a212a95fe147ebedf3fc47
Branden Wood <brandenek03912@hotmail.com>
2023-12-21 09:38:56 -0500

    inventory: add all owned items

--- BlasII.Randomizer.Map/InventoryHandler.cs
+++ BlasII.Randomizer.Map/InventoryHandler.cs
@@ -1,5 +1,5 @@
 ﻿using BlasII.Randomizer.Items;
-using UnityEngine;
+using System.Linq;
 
 namespace BlasII.Randomizer.Map
 {
@@ -33,14 +33,31 @@ namespace BlasII.Randomizer.Map
         private void CalculateInventory()
         {
             Main.MapTracker.Log("Calculating new inventory");
-            var randomizer = BlasII.Randomizer.Main.Randomizer;
+            _currentInventory = new Blas2Inventory(Main.Randomizer.CurrentSettings, Main.Randomizer.Data.DoorDictionary);
 
-            _currentInventory = new Blas2Inventory(randomizer.CurrentSettings, randomizer.Data.DoorDictionary);
-
-            if (Random.Range(0, 2) == 0)
+            foreach (var item in Main.Randomizer.Data.ItemList)
             {
-                _currentInventory.AddItem(Main.Randomizer.Data.GetItem("AB02"));
-                Main.MapTracker.Log("Adding dbl jump");
+                // Only check progression items
+                if (!item.progression)
+                    continue;
+
+                // Find the amount the player has of this item
+                int amount;
+                if (item.subItems == null)
+                {
+                    amount = Main.Randomizer.ItemHandler.CollectedItems.Count(x => x == item.id);
+                }
+                else
+                {
+                    amount = item.subItems.Count(x => Main.Randomizer.ItemHandler.IsItemCollected(x));
+                }
+
+                // Add it to the inventory
+                Main.MapTracker.LogWarning($"Adding {item.id} {amount} times!");
+                for (int i = 0; i < amount; i++)
+                {
+                    _currentInventory.AddItem(item);
+                }
             }
         }
     }
