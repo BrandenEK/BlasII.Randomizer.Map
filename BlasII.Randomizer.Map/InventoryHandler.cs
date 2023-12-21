976a7f5b91981c183fc7e93c92e88f6cf7aaf194
Branden Wood <brandenek03912@hotmail.com>
2023-12-21 09:52:17 -0500

    inventory: calculate based on collected locations instead

--- BlasII.Randomizer.Map/InventoryHandler.cs
+++ BlasII.Randomizer.Map/InventoryHandler.cs
@@ -1,5 +1,4 @@
 ﻿using BlasII.Randomizer.Items;
-using System.Linq;
 
 namespace BlasII.Randomizer.Map
 {
@@ -33,32 +32,54 @@ namespace BlasII.Randomizer.Map
         private void CalculateInventory()
         {
             Main.MapTracker.Log("Calculating new inventory");
-            _currentInventory = new Blas2Inventory(Main.Randomizer.CurrentSettings, Main.Randomizer.Data.DoorDictionary);
 
-            foreach (var item in Main.Randomizer.Data.ItemList)
+            var settings = Main.Randomizer.CurrentSettings;
+            _currentInventory = new Blas2Inventory(settings, Main.Randomizer.Data.DoorDictionary);
+            _currentInventory.AddItem(Main.Randomizer.Data.GetItem(GetStartingWeaponId(settings)));
+
+            foreach (string locationId in Main.Randomizer.ItemHandler.CollectedLocations)
             {
-                // Only check progression items
-                if (!item.progression)
-                    continue;
-
-                // Find the amount the player has of this item
-                int amount;
-                if (item.subItems == null)
-                {
-                    amount = Main.Randomizer.ItemHandler.CollectedItems.Count(x => x == item.id);
-                }
-                else
-                {
-                    amount = item.subItems.Count(x => Main.Randomizer.ItemHandler.IsItemCollected(x));
-                }
-
-                // Add it to the inventory
-                Main.MapTracker.LogWarning($"Adding {item.id} {amount} times!");
-                for (int i = 0; i < amount; i++)
+                Item item = Main.Randomizer.ItemHandler.GetItemAtLocation(locationId);
+                if (item.progression)
                 {
                     _currentInventory.AddItem(item);
+                    Main.MapTracker.LogWarning("Adding " + item.id);
                 }
             }
+
+            //foreach (var item in Main.Randomizer.Data.ItemList)
+            //{
+            //    // Only check progression items
+            //    if (!item.progression)
+            //        continue;
+
+            //    // Find the amount the player has of this item
+            //    int amount;
+            //    if (item.subItems == null)
+            //    {
+            //        amount = Main.Randomizer.ItemHandler.CollectedItems.Count(x => x == item.id);
+            //    }
+            //    else
+            //    {
+            //        amount = item.subItems.Count(x => Main.Randomizer.ItemHandler.IsItemCollected(x));
+            //    }
+
+            //    // Add it to the inventory
+            //    for (int i = 0; i < amount; i++)
+            //    {
+            //        _currentInventory.AddItem(item);
+            //    }
+            //    if (amount > 0)
+            //        Main.MapTracker.LogWarning($"Adding {item.id} {amount} times!");
+            //}
+        }
+
+        /// <summary>
+        /// Calculates the item id of the chosen starting weapon
+        /// </summary>
+        private string GetStartingWeaponId(RandomizerSettings settings)
+        {
+            return "WE0" + (settings.RealStartingWeapon + 1);
         }
     }
 }
